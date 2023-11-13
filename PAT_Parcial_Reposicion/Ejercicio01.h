6ed5c3c2667681a9ebb58f13cb377fd3eff96f5e
Jose Garcia <57154305+Yipieight@users.noreply.github.com>
2023-11-12 19:55:56 -0600

    all excersice resolve

--- PAT_Parcial_Reposicion/Ejercicio01.h
+++ PAT_Parcial_Reposicion/Ejercicio01.h
@@ -6,4 +6,5 @@ class Ejercicio01
 {
 public:
 	Node<int>* addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2);
+	Node<int>* reverseList(Node<int>* head);
 };
--- PAT_Parcial_Reposicion/Ejercicio02.cpp
+++ PAT_Parcial_Reposicion/Ejercicio02.cpp
@@ -1,6 +1,34 @@
 #include "Ejercicio02.h"
 
-Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k)
-{
-    return nullptr;
+Node<char>* reverseSublist(Node<char>* start, Node<char>* end) {
+    Node<char>* prev = nullptr;
+    Node<char>* curr = start;
+
+    while (curr != end) {
+        Node<char>* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+
+    return prev;
+}
+
+Node<char>* Ejercicio02::reverseKGroup(Node<char>* head, int k) {
+    Node<char>* curr = head;
+    int count = 0;
+
+    while (curr != nullptr && count < k) {
+        curr = curr->next;
+        count++;
+    }
+
+    if (count < k) {
+        return head;
+    }
+
+    Node<char>* newHead = reverseSublist(head, curr);
+    head->next = reverseKGroup(curr, k);
+
+    return newHead;
 }
